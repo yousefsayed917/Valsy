7d8087d9d7be6ef86ed4ff63e572c889ea89d26d
yousefsayed917 <112081079+yousefsayed917@users.noreply.github.com>
2026-05-09 17:31:10 +0300

    Add missing WeatherForecast model for API template controller

---
+++ Valsy.Api/WeatherForecast.cs
@@ -0,0 +1,10 @@
+namespace Valsy.Api;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+}
